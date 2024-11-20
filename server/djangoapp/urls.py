18b2091106d46e9dc26e36d69352e39eb0a135b9
Onne164 <summaarum@gmail.com>
2024-11-20 06:32:09 -0500

    created a superuser, added new login view

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,18 +1,17 @@
-# Uncomment the imports before you add the code
-# from django.urls import path
+from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-# from . import views
+from django.views.generic import TemplateView
+from . import views
 
 app_name = 'djangoapp'
+
 urlpatterns = [
-    # # path for registration
+    # Tee logimise jaoks, mis viib login_user vaate juurde
+    path('login', views.login_user, name='login'),  # Teeb POST päringu kasutaja sisselogimiseks
 
-    # path for login
-    # path(route='login', view=views.login_user, name='login'),
+    # Reacti rakenduse login leht (seda serveeritakse index.html kaudu)
+    #path('react-login/', TemplateView.as_view(template_name="index.html"), name='login_react'),  # Teenus Reacti jaoks
 
-    # path for dealer reviews view
-
-    # path for add a review view
-
+    # Siia tuleb ülejäänud teed (näiteks registreerimine, ülevaated jne)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
